aaa6cc0d85981b53f78340ab1ef48511c707bac9
HoltkampN <32459759+HoltkampN@users.noreply.github.com>
2017-10-15 17:44:06 -0500

    Update Program.cs

--- RefactorKata/RefactorKata/Program.cs
+++ RefactorKata/RefactorKata/Program.cs
@@ -15,10 +15,8 @@ namespace RefactorKata
                 Console.WriteLine("This product is called: " + product.Name);
             }
         }
-
         private static IEnumerable<Product> GetProducts()
         {
-
             using (var conn =
                 new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;"))
             {
@@ -27,18 +25,11 @@ namespace RefactorKata
 
                 var reader = cmd.ExecuteReader();
                 var products = new List<Product>();
-
-
-
                 while (reader.Read())
                 {
-
                     var prod = new Product { Name = reader["Name"].ToString() };
                     products.Add(prod);
-
                 }
-
-
                 Console.WriteLine("Products Loaded");
                 return products;
             }
